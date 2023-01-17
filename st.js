6c0a78817bb75ac8ef60f74fa0b621d962ed6d46
Kamakura-D <dai.1118.k.xe@docomo.ne.jp>
2023-01-17 17:44:08 +0900

    '連絡先の追加とその他修正'

--- st.js
+++ st.js
@@ -1,7 +1,7 @@
 const responsive_menu_btn = document.querySelector('.responsive_btn');
 responsive_menu_btn.addEventListener('click', menuToggle);
-const responsive_header_nav = document.querySelector('.header_nav');
-responsive_header_nav.addEventListener('click', menuToggle);
+const responsive_header_nav_lists = document.querySelector('.header_nav_lists');
+responsive_header_nav_lists.addEventListener('click', menuToggle);
 
 function menuToggle() {
   const header_menu_detail = document.querySelector('.header_nav');
